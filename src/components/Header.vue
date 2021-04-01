d238db48c2916c87fcd4eca7c3ad8a11a522b57d
phucduccoffee <pduccoffee@gmail.com>
2021-04-01 13:36:38 +0700

    .

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
   <nav id="navHeader" class="navbar navbar-expand-lg bg-white fixed-top navbar-transparent" color-on-scroll="500">
     <div class="container">
       <div class="navbar-translate">
-        <a class="navbar-brand" href="/"><img id="imgLogo" src="/assets/img/logo/logo-white.png" style="width: 200px;"/></a>
+        <a class="navbar-brand" href="/"><img id="imgLogo" src="/assets/img/logo/logo-black.png" style="width: 200px;"/></a>
         <button
           class="navbar-toggler"
           type="button"
