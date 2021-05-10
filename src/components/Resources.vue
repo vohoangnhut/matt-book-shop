446ea6b46ea0d77c62e12570c26208ddd94aeb54
Vo Nhut <nhut.vo@one-line.com>
2021-05-10 14:44:29 +0700

    layout new page

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -11,48 +11,48 @@
       <div class="contact-content">
         <div class="container">
           
-          <div class="col-md-8 ml-auto mr-auto text-center">
+          <div class="col-md-7 ml-auto mr-auto">
             <h2 class="title">Checklist</h2>
           </div>
 
-          <div class="row" style="margin-bottom: 60px;">
-            <div class="col-md-5 ml-auto mr-auto" style="margin-top: 23px;">
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto" style="margin-top: 15px;">
               <a href="https://www.dropbox.com/sh/7ebhwpt27fqpbbl/AADQUZWrIH40CsDar7u1QC36a/Upload%20Documents/Checklist/Landlord%20-%20HDB%20flat.pdf?dl=0" target="_blank">Landlord - HDB Flat</a>
             </div>
           </div>
-          <div class="row" style="margin-bottom: 60px;">
-            <div class="col-md-5 ml-auto mr-auto" style="margin-top: 23px;">
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto" style="margin-top: 15px;">
               <a href="https://www.dropbox.com/sh/7ebhwpt27fqpbbl/AABoGenL4ahm65jKAnwoYAoDa/Upload%20Documents/Checklist/Landlord%20-%20Private%20residential%20property.pdf?dl=0" target="_blank">Landlord - Private Residential Property</a>
             </div>
           </div>
-          <div class="row" style="margin-bottom: 60px;">
-            <div class="col-md-5 ml-auto mr-auto" style="margin-top: 23px;">
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto" style="margin-top: 15px;">
               <a href="https://www.dropbox.com/sh/7ebhwpt27fqpbbl/AABW84ZilKNbLuwh_69MHtDda/Upload%20Documents/Checklist/Lease%20-%20Compliance%20to%20women%2039%20s%20charter%20%26%20immigration%20act%20%28version-2%29.pdf?dl=0" target="_blank">Lease - Compliance to Women 39 S Charter & Immigration Act (version-2)</a>
             </div>
           </div>
-          <div class="row" style="margin-bottom: 60px;">
-            <div class="col-md-5 ml-auto mr-auto" style="margin-top: 23px;">
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto" style="margin-top: 15px;">
               <a href="https://www.dropbox.com/sh/7ebhwpt27fqpbbl/AAA820pBJukHZqHgBTZhEbX7a/Upload%20Documents/Checklist/Tenant%20-%20HDB%20flat.pdf?dl=0" target="_blank">Tenant - HDB Flat</a>
             </div>
           </div>
-          <div class="row" style="margin-bottom: 60px;">
-            <div class="col-md-5 ml-auto mr-auto" style="margin-top: 23px;">
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto" style="margin-top: 15px;">
               <a href="https://www.dropbox.com/sh/7ebhwpt27fqpbbl/AABnQZoobPWYXY4cdZ_nhy9ma/Upload%20Documents/Checklist/Tenant%20-%20Private%20residential%20property.pdf?dl=0" target="_blank">Tenant - Private Residential Property</a>
             </div>
           </div>
             
 
-          <div class="col-md-8 ml-auto mr-auto text-center">
+          <div class="col-md-7 ml-auto mr-auto">
             <h2 class="title">Tenancy Agreement Template</h2>
           </div>
 
-          <div class="row" style="margin-bottom: 60px;">
-            <div class="col-md-5 ml-auto mr-auto" style="margin-top: 23px;">
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto" style="margin-top: 15px;">
               <a href="https://www.dropbox.com/sh/7ebhwpt27fqpbbl/AABGlzHyhvMwIb22nu8_S_2aa/Upload%20Documents/Tenancy%20Agreement%20Template/HDB%20flats.pdf?dl=0" target="_blank">HDB Flats</a>
             </div>
           </div>
-          <div class="row" style="margin-bottom: 60px;">
-            <div class="col-md-5 ml-auto mr-auto" style="margin-top: 23px;">
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto" style="margin-top: 15px;margin-bottom: 30px;">
               <a href="https://www.dropbox.com/sh/7ebhwpt27fqpbbl/AABIb6V43U4vwZe0NGMAF9jca/Upload%20Documents/Tenancy%20Agreement%20Template/Private%20residential%20properties.pdf?dl=0" target="_blank">Private Residential Properties</a>
             </div>
           </div>
